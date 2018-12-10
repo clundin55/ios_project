33468db5fca218f0490c74f5b694c373fe8f1bb2
Carl <carllundin55@gmail.com>
2018-12-09 17:05:58 -0800

    Refactored so we can use an array to populate all our POIs

--- ZagTours/ZagTours/Models/ZagMark.swift
+++ ZagTours/ZagTours/Models/ZagMark.swift
@@ -10,42 +10,23 @@ import Foundation
 import MapKit
 import GoogleMaps
 
-struct ZagMark {
+class ZagMark {
     var description: String
+    var googleMarkerDescription: String 
     var title: String
     var image: String
     var websiteLink: String
     var googleMarker: GMSMarker
+    var position: CLLocationCoordinate2D
+    
+    init(description: String, googleMarkerDescription: String, title: String, image: String, websiteLink: String, googleMarker: GMSMarker, position: CLLocationCoordinate2D) {
+        self.description = description
+        self.googleMarkerDescription = googleMarkerDescription
+        self.title = title
+        self.image = image
+        self.websiteLink = websiteLink
+        self.googleMarker = googleMarker
+        self.position = position
+    }
 }
 
-class ZagMarkWrapper {
-    var zagMarks: [ZagMark] = []
-    var currentZagMark: ZagMark? = nil
-    
-    func populateMapWithZagMarks(campusMap: GMSMapView) {
-        let guLat = 47.6671926
-        let guLong = -117.4045736
-        
-        let marker = GMSMarker()
-        marker.position = CLLocationCoordinate2D(latitude: guLat, longitude: guLong)
-        marker.title = "Gonzaga University"
-        marker.snippet = "Go zags"
-        marker.icon = GMSMarker.markerImage(with: .blue)
-        marker.map = campusMap
-        
-        let tempMark = ZagMark(description: "Temp", title: "TempTitle", image: "None", websiteLink: "No website", googleMarker: marker)
-        zagMarks.append(tempMark)
-    }
-    
-    func getCurrentMarkerFromTitle() -> ZagMark?{
-        return currentZagMark
-    }
-    
-    func setCurrentMarkerFromTitle(title: String){
-        for zagMark in zagMarks {
-            if zagMark.googleMarker.title == title {
-                currentZagMark = zagMark
-            }
-        }
-    }
-}
---
+++ ZagTours/ZagTours/Models/ZagMarkWrapper.swift
@@ -0,0 +1,51 @@
+//
+//  ZagMarkWrapper.swift
+//  ZagTours
+//
+//  Created by Carl Lundin on 12/9/18.
+//  Copyright © 2018 Carl Lundin. All rights reserved.
+//
+
+import Foundation
+import GoogleMaps
+import GooglePlaces
+
+class ZagMarkWrapper {
+    var zagMarks: [ZagMark] = []
+    var currentZagMark: ZagMark? = nil
+    
+    func populateMapWithZagMarks(campusMap: GMSMapView) {
+        for zagmark in zagMarks{
+            zagmark.googleMarker.map = campusMap
+        }
+    }
+    
+    func createMarkers(){
+        zagMarks = [ZagMark(description: "Temp", googleMarkerDescription: "Insert detailed description",title: "Gonzaga University", image: "None", websiteLink: "No website", googleMarker: GMSMarker(), position: CLLocationCoordinate2D(latitude: 47.6671926, longitude: -117.4045736))]
+        
+        for zagmark in zagMarks{
+            createGMapMarkerFromZagMark(zagmark: zagmark)
+        }
+    }
+    
+    func createGMapMarkerFromZagMark(zagmark: ZagMark) {
+        let newMarker = GMSMarker()
+        newMarker.icon = GMSMarker.markerImage(with: .blue)
+        newMarker.position = zagmark.position
+        newMarker.title = zagmark.title
+        newMarker.snippet = zagmark.googleMarkerDescription
+        zagmark.googleMarker = newMarker
+    }
+    
+    func getCurrentMarkerFromTitle() -> ZagMark?{
+        return currentZagMark
+    }
+    
+    func setCurrentMarkerFromTitle(title: String){
+        for zagMark in zagMarks {
+            if zagMark.googleMarker.title == title {
+                currentZagMark = zagMark
+            }
+        }
+    }
+}
