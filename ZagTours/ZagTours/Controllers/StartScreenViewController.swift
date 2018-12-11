e2fda7504ceb011ccc2e55ab18636ab528a2561a
Abrahms <dabrahms@HK220-Mac3.local>
2018-12-11 15:11:54 -0800

    Small Styling Changes:

--- ZagTours/ZagTours/Controllers/StartScreenViewController.swift
+++ ZagTours/ZagTours/Controllers/StartScreenViewController.swift
@@ -12,7 +12,7 @@ class StartScreenViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        self.view.backgroundColor = UIColor(patternImage: UIImage(named: "background.jpg")!)
         // Do any additional setup after loading the view.
     }
     
